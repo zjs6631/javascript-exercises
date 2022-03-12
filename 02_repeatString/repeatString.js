c3fc4423917b67b4686afc195ff95582dba14111
zjs6631 <zacharyjonessmith@gmail.com>
2022-03-12 16:24:45 -0500

    Finished exercise 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,17 @@
-const repeatString = function() {
 
+
+const repeatString = function(string, num) {
+    let holdString = ""
+    if (num >=0){
+        for (i = 1; i <= num; i++){
+            holdString += string;
+        }
+        return holdString;
+    } else{
+        return "ERROR"
+    }
 };
+
 
 // Do not edit below this line
 module.exports = repeatString;
