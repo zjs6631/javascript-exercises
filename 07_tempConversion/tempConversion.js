635cfa7c79e16f8a18fb0fad42584e7f91f051b6
zjs6631 <zacharyjonessmith@gmail.com>
2022-03-23 13:15:48 -0400

    completed tempConversion exercise javascript

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,11 @@
-const ftoc = function() {
-
+const ftoc = function(f) {
+  let c = (f-32) * (5/9);
+  return Math.round(c * 10)/10;
 };
 
-const ctof = function() {
-
+const ctof = function(c) {
+  let f = c * (9/5) + 32;
+  return Math.round(f * 10)/10;
 };
 
 // Do not edit below this line
