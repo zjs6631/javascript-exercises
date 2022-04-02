5064e4a4ca37805118caba22d23827e43268423b
zjs6631 <zacharyjonessmith@gmail.com>
2022-04-02 13:27:40 -0400

    completed exercise 8 for making a calculator function

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,48 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+	let res = num1 + num2;
+  return res;
+  
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+	let res = num1 - num2;
+  return res;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+	let total = 0;
+  for (i = 0; i < array.length; i++){
+    total+=array[i];
+  }
+  return total;
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  let total = 1;
+  for (i=0; i < array.length; i++){
+    total*=array[i];
+  }
+  return total;
 };
 
-const power = function() {
-	
+const power = function(num1, num2) {
+	let res = num1 ** num2;
+  return res;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+	let res = 1;
+  if (num == 1 || num == 0){
+    return res;
+  } 
+  else {
+    for (i = num; i > 0; i--){
+      res = res * i;
+    }
+    return res;
+  }
+  
+  
 };
 
 // Do not edit below this line
