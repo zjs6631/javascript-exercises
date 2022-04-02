a4673cb7a4164cb4b548504db07db1c758918b3a
zjs6631 <zacharyjonessmith@gmail.com>
2022-04-02 15:07:03 -0400

    finished exercise 11 titles. Accessed and used obj attrbs.

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,4 +1,12 @@
-const getTheTitles = function() {
+const getTheTitles = function(arr) {
+    let titles = []; //create new array to hold results
+    for (i = 0; i < arr.length; i++){ //use for loop to loop through
+        //provided array
+        titles[i] = arr[i]["title"]; //set each index to the value in the title attribute
+        //for each obj
+    }
+    
+    return titles; //return the array
 
 };
 
