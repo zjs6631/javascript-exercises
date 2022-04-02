d06126bb2155fd39db2e512efa1caad62a0f7651
zjs6631 <zacharyjonessmith@gmail.com>
2022-04-02 14:40:29 -0400

    finished exercise 10 fibonacci. have previously worked on this problem/seen this algo, so not new con

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,4 +1,16 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+    num = parseInt(num); //turn input into a int (to allow strings)
+    if (num < 1){
+        return "OOPS"; //if statement to make sure value is positive
+    }
+    if (num == 1 || num == 2){
+        return 1; //if input is 1 or 2 then fib number is 1
+    } else {
+        return fibonacci(num-1) + fibonacci(num-2); //else recursively call
+        //fib function on the previous numbers
+        //creates a call stack and then pops off the top of the stack
+        //to return the answer
+    }
 
 };
 
